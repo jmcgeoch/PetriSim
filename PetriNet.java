c2c3ee3dc21a71956c2fe5cb25980574b8ee1df3
Jessica Zemartis <jessica.a.zemartis@monmouth.edu>
2017-12-05 21:48:43 -0500

    Small GUI Insertion
    Just added some GUI code to the main class to start learning how to add
    GUI functionality to the project

--- PetriNet.java
+++ PetriNet.java
@@ -1,12 +1,56 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
-public class PetriNet {
-
+public class PetriNet extends Application{
+	
+	@Override
+	public void start(Stage primaryStage) {
+		//create the scene and place two buttons in the scene
+		HBox pane = new HBox(10);
+		pane.setAlignment(Pos.CENTER);
+		
+		Button runButton = new Button("Run");
+		Button delButton = new Button("Delete");
+		
+		RunHandlerClass handler1 = new RunHandlerClass();
+		runButton.setOnAction((EventHandler<ActionEvent>) handler1);
+		
+		DelHandlerClass handler2 = new DelHandlerClass();
+		delButton.setOnAction(handler2);
+		
+		pane.getChildren().addAll(runButton, delButton);
+		
+		//define scene params
+		Scene scene = new Scene(pane, 400, 300);
+		primaryStage.setTitle("Petri Net Simulation");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	 
+	class RunHandlerClass implements EventHandler<ActionEvent> {
+		public void handle(ActionEvent e) {
+			System.out.println("Run Clicked");
+		}
+	}
+	
+	class DelHandlerClass implements EventHandler<ActionEvent> {
+		public void handle(ActionEvent e) {
+			System.out.println("Delete Clicked");
+		}
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Application.launch(args);
 	}
 
 }
